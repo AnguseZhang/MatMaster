38257cc80698c7c33c71ebeeb12f808dac4364cc
Junshang Zhang <anton_616@qq.com>
2025-08-10 13:27:45 +0800

    feat:add HEA_assistant_agent

--- agents/matmaster_agent/agent.py
+++ agents/matmaster_agent/agent.py
@@ -1,32 +1,23 @@
 from google.adk.agents import LlmAgent
 from opik.integrations.adk import track_adk_agent_recursive
 
+from agents.matmaster_agent.ABACUS_agent.agent import init_abacus_calculation_agent
+from agents.matmaster_agent.DPACalculator_agent.agent import init_dpa_calculations_agent
+from agents.matmaster_agent.INVAR_agent.agent import init_invar_agent
 from agents.matmaster_agent.apex_agent.agent import init_apex_agent
 from agents.matmaster_agent.base_agents.io_agent import (
     HandleFileUploadLlmAgent,
 )
 from agents.matmaster_agent.callback import matmaster_before_agent
 from agents.matmaster_agent.constant import MATMASTER_AGENT_NAME
-from agents.matmaster_agent.DPACalculator_agent.agent import (
-    init_dpa_calculations_agent,
-)
-from agents.matmaster_agent.llm_config import MatMasterLlmConfig
-
-from agents.matmaster_agent.piloteye_electro_agent.agent import init_piloteye_electro_agent
-from agents.matmaster_agent.DPACalculator_agent.agent import init_dpa_calculations_agent
-from agents.matmaster_agent.thermoelectric_agent.agent import init_thermoelectric_agent
-from agents.matmaster_agent.optimade_database_agent.agent import init_optimade_database_agent
-from agents.matmaster_agent.superconductor_agent.agent import init_superconductor_agent
-from agents.matmaster_agent.HEA_assistant_agent.agent import init_HEA_assistant_agent
 from agents.matmaster_agent.crystalformer_agent.agent import init_crystalformer_agent
-from agents.matmaster_agent.ABACUS_agent.agent import init_abacus_calculation_agent
+from agents.matmaster_agent.llm_config import MatMasterLlmConfig
 from agents.matmaster_agent.optimade_database_agent.agent import (
     init_optimade_database_agent,
 )
 from agents.matmaster_agent.piloteye_electro_agent.agent import (
     init_piloteye_electro_agent,
 )
-
 from agents.matmaster_agent.prompt import (
     AgentDescription,
     AgentInstruction,
@@ -41,8 +32,12 @@ from agents.matmaster_agent.thermoelectric_agent.agent import (
 from agents.matmaster_agent.traj_analysis_agent.agent import (
     init_traj_analysis_agent,
 )
-
-
+from agents.matmaster_agent.organic_reaction_agent.agent import (
+    init_organic_reaction_agent,
+)
+from agents.matmaster_agent.HEA_assistant_agent.agent import (
+    init_HEA_assistant_agent
+)
 class MatMasterAgent(HandleFileUploadLlmAgent):
 
     def __init__(self, llm_config):
@@ -52,9 +47,11 @@ class MatMasterAgent(HandleFileUploadLlmAgent):
         dpa_calculator_agent = init_dpa_calculations_agent(llm_config)
         thermoelectric_agent = init_thermoelectric_agent(llm_config)
         superconductor_agent = init_superconductor_agent(llm_config)
+        invar_agent = init_invar_agent(llm_config)
         crystalformer_agent = init_crystalformer_agent(llm_config)
         apex_agent = init_apex_agent(llm_config, use_deepseek=True)
         abacus_calculator_agent = init_abacus_calculation_agent(llm_config)
+        organic_reaction_agent = init_organic_reaction_agent(llm_config)
         HEA_assistant_agent = init_HEA_assistant_agent(llm_config)
 
         super().__init__(
@@ -70,6 +67,8 @@ class MatMasterAgent(HandleFileUploadLlmAgent):
                 apex_agent,
                 crystalformer_agent,
                 abacus_calculator_agent,
+                invar_agent,
+                organic_reaction_agent,
                 HEA_assistant_agent
             ],
             global_instruction=GlobalInstruction,
@@ -80,10 +79,10 @@ class MatMasterAgent(HandleFileUploadLlmAgent):
 
 
 def init_matmaster_agent() -> LlmAgent:
-    matmodeler_agent = MatMasterAgent(MatMasterLlmConfig)
-    track_adk_agent_recursive(matmodeler_agent, MatMasterLlmConfig.opik_tracer)
+    matmaster_agent = MatMasterAgent(MatMasterLlmConfig)
+    track_adk_agent_recursive(matmaster_agent, MatMasterLlmConfig.opik_tracer)
 
-    return matmodeler_agent
+    return matmaster_agent
 
 
 # Global instance of the agent
