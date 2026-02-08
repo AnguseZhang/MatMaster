ca793a62223e5d759174bf183f57c35bd0d26fed
styxhuang <huangmingwilliam@gmail.com>
2026-02-08 08:52:23 +0800

    fix: agent name

--- agents/matmaster_agent/sub_agents/structure_search_agent/constant.py
+++ agents/matmaster_agent/sub_agents/structure_search_agent/constant.py
@@ -1,6 +1,6 @@
 from agents.matmaster_agent.constant import CURRENT_ENV
 
-STRUCTURE_SEARCH_AGENT_NAME = 'fetch_structures_from_db'
+STRUCTURE_SEARCH_AGENT_NAME = 'structure_search_agent'
 
 if CURRENT_ENV in ['test', 'uat']:
     STRUCTURE_SEARCH_URL = 'http://chvz1424099.bohrium.tech:50001/sse'
